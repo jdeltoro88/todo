76c059c13169636043eeca977a249d8524ef1ccc
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2022-10-18 16:31:45 -0600

    initiated component folder, form code in place

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import './App.css';
+//component imports
+import Form from "./components/Form";
 
 function App() {
   return (
@@ -7,6 +9,7 @@ function App() {
       <header>
       <h1>hello</h1>
       </header>
+      <Form/>
     </div>
   );
 }
