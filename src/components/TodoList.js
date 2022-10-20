4c992ac222bd051e82c1f430010546ca83cd4de1
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2022-10-20 11:13:19 -0600

    todolist initiated

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 const TodoList = () => {
   return (
-    <div>TodoList</div>
-  )
+    <div class="todo-container">
+      <ul class="todo-list"></ul>
+    </div>
+  );
 }
 
 export default TodoList
