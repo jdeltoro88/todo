5303dba673394ef42b4feb2d5cb3a9cbe9b09efc
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2022-11-01 19:12:01 -0600

    todo buttons rendered

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,8 +2,16 @@ import React from 'react'
 
 const Todo = () => {
   return (
-    <div>Todo</div>
-  )
+    <div className="todo">
+      <li className="todo-item">item</li>
+      <button className="complete-btn">
+        <i className="fas fa-check"></i>
+      </button>
+      <button className="trash-btn">
+        <i className="fas fa-trash"></i>
+      </button>
+    </div>
+  );
 }
 
 export default Todo
