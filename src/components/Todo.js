a1c23ab601579cd4b5ca960bb3f94444e3d61951
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2022-11-17 18:26:01 -0700

    status

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Todo = ({ text,todo,  todos, setTodos }) => {
+const Todo = ({ text, todo,  todos, setTodos }) => {
     //eventhandler
     const deleteHandler = () => {
         setTodos(todos.filter((el) => el.id !== todo.id))
