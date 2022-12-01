b332c31517454794891a3fd4f1c50871fe5111ba
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2022-11-30 19:57:36 -0700

    local save working

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect  } from "react";
 import './App.css';
 //component imports 
 import Form from "./components/Form";
@@ -10,7 +10,18 @@ function App() {
   const [todos, setTodos] = useState([]);
   const [status, setStatus] = useState('all');
   const [filteredTodos, setFilteredTodos] = useState([]);
+
+  //run once on app start
+  useEffect(() => {
+    getLocalTodos();
+  }, [])
+  //useEffect
+  useEffect(() => {
+    filterHandler();
+    saveLocalTodos();
+  }, [todos, status]);
   //functions
+
   const filterHandler = () => {
     switch(status) {
       case 'completed':
@@ -23,21 +34,41 @@ function App() {
             setFilteredTodos(todos);
             break; 
     }
-  }
+  };
+
+  //local save
+  const saveLocalTodos = () => {
+      localStorage.setItem('todos', JSON.stringify(todos))
+    };
+  
+  const getLocalTodos = () => {
+    if(localStorage.getItem('todos') === null) {
+      localStorage.setItem('todos', JSON.stringify([]));
+    } else {
+      let todoLocal = JSON.parse(localStorage.getItem)('todos')
+      setTodos(todoLocal);
+    }
+  };
+
+
   return (
     <div className="App">
       <header>
-      <h1>hello</h1>
+        <h1>hello</h1>
       </header>
-      <Form 
-      inputText={inputText}
-      todos={todos} 
-      setTodos={setTodos} 
-      setInputText={setInputText}
-      setStatus={setStatus} 
+      <Form
+        inputText={inputText}
+        todos={todos}
+        setTodos={setTodos}
+        setInputText={setInputText}
+        setStatus={setStatus}
       />
 
-      <TodoList setTodos={setTodos} todos={todos}/>
+      <TodoList
+        filteredTodos={filteredTodos}
+        setTodos={setTodos}
+        todos={todos}
+      />
     </div>
   );
 }
